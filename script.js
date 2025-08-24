e896f1c11ad832b52c7814afeab176ba084258e0
anshikaa02 <anshika.sharma0027@gmail.com>
2025-08-24 21:24:37 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+document.querySelector("form").addEventListener("submit", () => {
+    alert("Thank you! Your message has been received.");
+  });
+  function toggleTheme() {
+    document.body.classList.toggle("dark-mode");
+  }
+  
